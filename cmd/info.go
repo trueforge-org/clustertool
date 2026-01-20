b9dc529d64870e0359e5e03990e3e0d731904c7d
Kjeld Schouten <info@kjeldschouten.nl>
2026-01-20 20:28:59 +0100

    place back cmd

---
+++ cmd/info.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/trueforge-org/forgetool/pkg/info"
+)
+
+var infoLongHelp = strings.TrimSpace(`
+clustertool is a tool to help you easily deploy and maintain a Talos Kubernetes Cluster.
+
+
+Workflow:
+  Create talconfig.yaml file defining your nodes information like so:
+
+ Available commands
+  > clustertool init
+  > clustertool genconfig
+
+`)
+
+var infoCmd = &cobra.Command{
+	Use:     "info",
+	Short:   "Prints information about the clustertool binary",
+	Long:    infoLongHelp,
+	Example: "clustertool info",
+	Run: func(cmd *cobra.Command, args []string) {
+		info.NewInfo().Print()
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(infoCmd)
+}
