9f316e5aa3a5cc85522df64bfc870c8d571507b7
Boemeltrein <130394941+Boemeltrein@users.noreply.github.com>
2026-05-01 08:35:23 +0200

    Hmmm Don;t understand
    Signed-off-by: Boemeltrein <130394941+Boemeltrein@users.noreply.github.com>

--- pkg/gencmd/upgrade.go
+++ pkg/gencmd/upgrade.go
@@ -21,8 +21,7 @@ func GenUpgrade(node string, extraFlags []string) []string {
 	r, w, _ := os.Pipe()
 	os.Stdout = w
 	extraFlags = append(extraFlags, "--preserve")
-	// err := generate.GenerateUpgradeCommand(talassist.TalConfig, helper.TalosGenerated, node, extraFlags, false)  REMOVE LAST FALSE
-	err := generate.GenerateUpgradeCommand(talassist.TalConfig, helper.TalosGenerated, node, extraFlags)
+	err := generate.GenerateUpgradeCommand(talassist.TalConfig, helper.TalosGenerated, node, extraFlags, false)
 	
 	w.Close()
 	out, _ := io.ReadAll(r)
